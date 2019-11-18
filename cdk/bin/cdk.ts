94e2cad1ae78455498c9e807cc8bc7f3344f30c3
Steffen Hausmann <shausma@amazon.de>
2019-11-18 04:32:18 -0500

    Add CDK template to build workshop CFN template
    Reference the new template location in the workshop

--- cdk/bin/cdk.ts
+++ cdk/bin/cdk.ts
@@ -1,10 +1,17 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
-import { CdkStack } from '../lib/cdk-stack';
+import { WorkshopInfrastructure } from '../lib/workshop-infrastructure';
+import { BuildWorkshopResources } from '../lib/build-workshop-resources';
 
 const app = new cdk.App();
 
-new CdkStack(app, 'StreamingAnalyticsWorkshop', {
+new WorkshopInfrastructure(app, 'StreamingAnalyticsWorkshop', {
     description: 'Creates all resources and compiles all artifacts that are required to run the streaming analytics workshop (shausma-streaming-analytics-workshop-self-paced)'
 });
+
+new WorkshopInfrastructure(app, 'StreamingAnalyticsWorkshopEventEngine', {
+    description: 'Creates all resources and compiles all artifacts that are required to run the streaming analytics workshop (shausma-streaming-analytics-workshop-event-engine)'
+});
+
+new BuildWorkshopResources(app, 'BuildStreamingAnalyticsWorkshopResources');
