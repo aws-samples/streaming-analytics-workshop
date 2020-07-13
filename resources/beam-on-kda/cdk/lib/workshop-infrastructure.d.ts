cc61fbb50d9c1fac01044782a5e2d03a1a5057bc
Steffen Hausmann <shausma@amazon.de>
2020-07-13 15:57:39 +0200

    Add gitignore to cdk folder and remove (now) ignored files

--- resources/beam-on-kda/cdk/lib/workshop-infrastructure.d.ts
+++
@@ -1,9 +0,0 @@
-import cdk = require('@aws-cdk/core');
-export interface WorkshopInfrastructureProps extends cdk.StackProps {
-    kinesisReplayVersion: String;
-    beamApplicationVersion: String;
-    appName: string;
-}
-export declare class WorkshopInfrastructure extends cdk.Stack {
-    constructor(scope: cdk.Construct, id: string, props: WorkshopInfrastructureProps);
-}
