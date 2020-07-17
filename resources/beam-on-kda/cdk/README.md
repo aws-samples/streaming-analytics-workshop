328ba51b669d4667b7a509f30d7a357d3a9b07c0
Rajan Patel <rpatelzn@amazon.co.uk>
2020-07-17 12:50:39 +0100

    Added notes to cdk deploymeny and updated Lambda type: FirehoseTranformationLambda

--- resources/beam-on-kda/cdk/README.md
+++ resources/beam-on-kda/cdk/README.md
@@ -1,8 +1,58 @@
 # Useful commands
 
- * `npm run build`   compile typescript to js
- * `npm run watch`   watch for changes and compile
- * `npm run test`    perform the jest unit tests
- * `cdk deploy`      deploy this stack to your default AWS account/region
- * `cdk diff`        compare deployed stack with current state
- * `cdk synth`       emits the synthesized CloudFormation template
+- `npm run build` compile typescript to js
+- `npm run watch` watch for changes and compile
+- `npm run test` perform the jest unit tests
+- `cdk deploy` deploy this stack to your default AWS account/region
+- `cdk diff` compare deployed stack with current state
+- `cdk synth` emits the synthesized CloudFormation template
+
+## First time setup with local CDK Deployment
+
+To set up local CDK and deploy against an AWS profile of your choice, need to install CDK and install dependencies. Once this is done run cdk deploy, note the cf file is > 50KiB so need to upload to a S3 bucket first. The CDK deploy takes about 10 minutes to run.
+
+- Follow this link to install [AWS CDK](https://docs.aws.amazon.com/cdk/latest/guide/getting_started.html)
+- Add your AWS Account and credentials as a profile to `~/.aws/credentials` and `~/.aws/config`
+
+```bash
+~/.aws/credentials
+[myprofile]
+aws_access_key_id = MY_AWS_ACCESSKEY
+aws_secret_access_key = MY_AWS_SECRETKEY
+aws_session_token = MY_AWS_SESSION_TOKEN
+
+~/.aws/config
+[profile myprofile]
+output = json
+region = eu-west-1
+```
+
+Once this is setup run the following to deploy:
+
+```bash
+$ cd resources/cdk
+$ cdk --version
+1.51.0 (build 8c2d53c)
+
+1. Setup npm dependencies
+$ npm install
+$ npm watch
+
+2. Deploy the CDK stack
+$ cdk bootstrap aws://[AWSACCOUNTID]/eu-west-1 --profile myprofile
+$ cdk deploy --profile myprofile
+...
+
+The following o/p will be created upon completion - check the cloud formation stack in aws account
+
+beam-workshop.WindowsDevEnvironmentInstanceIp9C0E1056 =
+beam-workshop.WindowsDevEnvironmentInstanceLoginCredentials9CFB9646 =
+beam-workshop.S3Bucket =
+beam-workshop.ApplicationTerminatedTopicName =
+beam-workshop.InputS3Pattern =
+beam-workshop.FirehoseTransformationLambda =
+beam-workshop.BeamConsumerJarPath = target/amazon-kinesis-analytics-beam-taxi-consumer-1.0-SNAPSHOT.jar
+
+3. For Diffs run
+$ cdk diff --profile myprofile
+```
--- resources/beam-on-kda/cdk/lib/workshop-infrastructure.ts
+++ resources/beam-on-kda/cdk/lib/workshop-infrastructure.ts
@@ -1,114 +1,123 @@
-import fs = require('fs');
-import cdk = require('@aws-cdk/core');
-import s3 = require('@aws-cdk/aws-s3');
-import iam = require('@aws-cdk/aws-iam');
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import lambda = require('@aws-cdk/aws-lambda');
-import { Duration } from '@aws-cdk/core';
-import { EmptyBucketOnDelete } from './empty-bucket';
-import { GithubBuildPipeline } from './github-build-pipeline';
-import { WindowsDevEnvironment } from './windows-dev-environment';
-
+import fs = require("fs");
+import cdk = require("@aws-cdk/core");
+import s3 = require("@aws-cdk/aws-s3");
+import iam = require("@aws-cdk/aws-iam");
+import sns = require("@aws-cdk/aws-sns");
+import subs = require("@aws-cdk/aws-sns-subscriptions");
+import lambda = require("@aws-cdk/aws-lambda");
+import { Duration } from "@aws-cdk/core";
+import { EmptyBucketOnDelete } from "./empty-bucket";
+import { GithubBuildPipeline } from "./github-build-pipeline";
+import { WindowsDevEnvironment } from "./windows-dev-environment";
 
 export interface WorkshopInfrastructureProps extends cdk.StackProps {
-  kinesisReplayVersion: string,
-  beamApplicationVersion: string,
-  beamApplicationJarFile: string,
-  appName: string,
+  kinesisReplayVersion: string;
+  beamApplicationVersion: string;
+  beamApplicationJarFile: string;
+  appName: string;
 }
 
 export class WorkshopInfrastructure extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props: WorkshopInfrastructureProps) {
+  constructor(
+    scope: cdk.Construct,
+    id: string,
+    props: WorkshopInfrastructureProps
+  ) {
     super(scope, id, props);
 
-
-    const bucket = new s3.Bucket(this, 'Bucket', {
-      versioned: true
+    const bucket = new s3.Bucket(this, "Bucket", {
+      versioned: true,
     });
 
-    const emptyBucket = new EmptyBucketOnDelete(this, 'EmptyBucket', {
-      bucket: bucket
+    const emptyBucket = new EmptyBucketOnDelete(this, "EmptyBucket", {
+      bucket: bucket,
     });
 
-    new cdk.CfnOutput(this, 'S3Bucket', { value: bucket.bucketName });
-    new cdk.CfnOutput(this, 'InputS3Pattern', { value: `s3://${bucket.bucketName}/historic-trip-events/*/*/*/*/*` });
-    
+    new cdk.CfnOutput(this, "S3Bucket", { value: bucket.bucketName });
+    new cdk.CfnOutput(this, "InputS3Pattern", {
+      value: `s3://${bucket.bucketName}/historic-trip-events/*/*/*/*/*`,
+    });
 
-
-    new WindowsDevEnvironment(this, 'WindowsDevEnvironment', {
+    new WindowsDevEnvironment(this, "WindowsDevEnvironment", {
       ...props,
       bucket: bucket,
     });
 
-
-    new GithubBuildPipeline(this, 'BeamConsumerBuildPipeline', {
+    new GithubBuildPipeline(this, "BeamConsumerBuildPipeline", {
       url: `https://github.com/aws-samples/amazon-kinesis-analytics-beam-taxi-consumer/archive/${props.beamApplicationVersion}.zip`,
       bucket: bucket,
-      extract: true
+      extract: true,
     });
 
-    new cdk.CfnOutput(this, 'BeamConsumerJarPath', { value: `target/${props.beamApplicationJarFile}` });
+    new cdk.CfnOutput(this, "BeamConsumerJarPath", {
+      value: `target/${props.beamApplicationJarFile}`,
+    });
 
+    const addTimestamplambdaSource = fs
+      .readFileSync("lambda/add-approximate-arrival-time.js")
+      .toString();
 
-    
-
-    const addTimestamplambdaSource = fs.readFileSync('lambda/add-approximate-arrival-time.js').toString();
-
-    const enrichEvents = new lambda.Function(this, 'EnrichEventsLambda', {
+    const enrichEvents = new lambda.Function(this, "EnrichEventsLambda", {
       runtime: lambda.Runtime.NODEJS_12_X,
       code: lambda.Code.inline(addTimestamplambdaSource),
       timeout: Duration.seconds(60),
-      handler: 'index.handler'
+      handler: "index.handler",
     });
 
-    new cdk.CfnOutput(this, 'FirehoseTranformationLambda', { value: enrichEvents.functionName });
+    new cdk.CfnOutput(this, "FirehoseTransformationLambda", {
+      value: enrichEvents.functionName,
+    });
 
+    const stopApplicationlambdaSource = fs
+      .readFileSync("lambda/stop-kda-app.py")
+      .toString();
 
-
-    const stopApplicationlambdaSource = fs.readFileSync('lambda/stop-kda-app.py').toString();
-
-    const terminateAppLambda =  new lambda.Function(this, 'TerminateAppLambda', {
-        runtime: lambda.Runtime.PYTHON_3_7,
-        timeout: Duration.minutes(15),
-        code: lambda.Code.inline(stopApplicationlambdaSource),
-        handler: 'index.empty_bucket',
-        memorySize: 512,
-        environment: {
-            application_name: props.appName
-        }
+    const terminateAppLambda = new lambda.Function(this, "TerminateAppLambda", {
+      runtime: lambda.Runtime.PYTHON_3_7,
+      timeout: Duration.minutes(15),
+      code: lambda.Code.inline(stopApplicationlambdaSource),
+      handler: "index.empty_bucket",
+      memorySize: 512,
+      environment: {
+        application_name: props.appName,
+      },
     });
 
     terminateAppLambda.addToRolePolicy(
-        new iam.PolicyStatement({
-            actions: ['kinesisanalytics:StopApplication'],
-            resources: [`arn:${cdk.Aws.PARTITION}:kinesisanalytics:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:application/${props.appName}`]
-        })
+      new iam.PolicyStatement({
+        actions: ["kinesisanalytics:StopApplication"],
+        resources: [
+          `arn:${cdk.Aws.PARTITION}:kinesisanalytics:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:application/${props.appName}`,
+        ],
+      })
     );
 
-    const topic = new sns.Topic(this, 'ApplicationTerminatedTopic');
+    const topic = new sns.Topic(this, "ApplicationTerminatedTopic");
 
     topic.addSubscription(new subs.LambdaSubscription(terminateAppLambda));
 
-    new cdk.CfnOutput(this, 'ApplicationTerminatedTopicName', { value: topic.topicName });
-
-
-
-
-    const kdaRole = new iam.Role(this, 'KdaRole', {
-        assumedBy: new iam.ServicePrincipal('kinesisanalytics.amazonaws.com'),
-
+    new cdk.CfnOutput(this, "ApplicationTerminatedTopicName", {
+      value: topic.topicName,
     });
 
-    kdaRole.addToPolicy(new iam.PolicyStatement({
+    const kdaRole = new iam.Role(this, "KdaRole", {
+      assumedBy: new iam.ServicePrincipal("kinesisanalytics.amazonaws.com"),
+    });
+
+    kdaRole.addToPolicy(
+      new iam.PolicyStatement({
         actions: [
-        'logs:Describe*', 'logs:PutLogEvents',
-        'kinesis:List*', 'kinesis:Describe*', 'kinesis:Get*', 'kinesis:SubscribeToShard',
+          "logs:Describe*",
+          "logs:PutLogEvents",
+          "kinesis:List*",
+          "kinesis:Describe*",
+          "kinesis:Get*",
+          "kinesis:SubscribeToShard",
         ],
-        resources: [ '*' ]
-    }));
+        resources: ["*"],
+      })
+    );
 
     bucket.grantRead(kdaRole);
-
   }
 }
