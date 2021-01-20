3ec988a20bad1cdf28308d1a39ad54b054f69430
Steffen Hausmann <shausma@amazon.de>
2021-01-20 15:53:21 +0100

    Update workshop to Flink 1.11

--- resources/flink-on-kda/cdk/bin/cdk.ts
+++ resources/flink-on-kda/cdk/bin/cdk.ts
@@ -10,18 +10,18 @@ const synthDate = new Date().toISOString().split('T')[0];
 
 new WorkshopInfrastructure(app, 'StreamingAnalyticsWorkshop', {
     description: `Creates all resources and compiles all artifacts that are required to run the streaming analytics workshop (shausma-streaming-analytics-workshop-self-paced-${synthDate})`,
-    flinkVersion: '1.8.2',
+    flinkVersion: '1.11.2',
     flinkScalaVersion: '2.11',
-    kinesisReplayVersion: 'release-0.1.0',
+    kinesisReplayVersion: 'release-0.1.1',
     consumerApplicationVersion: 'release-0.2.0',
     consumerApplicationJarObject: 'amazon-kinesis-analytics-taxi-consumer-0.2.0.jar'
 });
 
 new WorkshopInfrastructure(app, 'StreamingAnalyticsWorkshopEventEngine', {
     description: `Creates all resources and compiles all artifacts that are required to run the streaming analytics workshop (shausma-streaming-analytics-workshop-event-engine-${synthDate})`,
-    flinkVersion: '1.8.2',
+    flinkVersion: '1.11.2',
     flinkScalaVersion: '2.11',
-    kinesisReplayVersion: 'release-0.1.0',
+    kinesisReplayVersion: 'release-0.1.1',
     consumerApplicationVersion: 'release-0.2.0',
     consumerApplicationJarObject: 'amazon-kinesis-analytics-taxi-consumer-0.2.0.jar'
 });
