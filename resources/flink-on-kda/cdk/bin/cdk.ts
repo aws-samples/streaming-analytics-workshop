404255ea68114331484d09123f0efd7552460b31
Steffen Hausmann <shausma@amazon.de>
2021-05-20 16:19:09 +0200

    Bump version of amazon-kinesis-analytics-taxi-consumer dependency

--- resources/flink-on-kda/cdk/bin/cdk.ts
+++ resources/flink-on-kda/cdk/bin/cdk.ts
@@ -11,15 +11,15 @@ const synthDate = new Date().toISOString().split('T')[0];
 new WorkshopInfrastructure(app, 'StreamingAnalyticsWorkshop', {
     description: `Creates all resources and compiles all artifacts that are required to run the streaming analytics workshop (shausma-streaming-analytics-workshop-self-paced-${synthDate})`,
     kinesisReplayVersion: 'release-0.1.0',
-    consumerApplicationVersion: 'release-0.2.0',
-    consumerApplicationJarObject: 'amazon-kinesis-analytics-taxi-consumer-0.2.0.jar'
+    consumerApplicationVersion: 'release-0.2.1',
+    consumerApplicationJarObject: 'amazon-kinesis-analytics-taxi-consumer-0.2.1.jar'
 });
 
 new WorkshopInfrastructure(app, 'StreamingAnalyticsWorkshopEventEngine', {
     description: `Creates all resources and compiles all artifacts that are required to run the streaming analytics workshop (shausma-streaming-analytics-workshop-event-engine-${synthDate})`,
     kinesisReplayVersion: 'release-0.1.0',
-    consumerApplicationVersion: 'release-0.2.0',
-    consumerApplicationJarObject: 'amazon-kinesis-analytics-taxi-consumer-0.2.0.jar'
+    consumerApplicationVersion: 'release-0.2.1',
+    consumerApplicationJarObject: 'amazon-kinesis-analytics-taxi-consumer-0.2.1.jar'
 });
 
 new BuildWorkshopResources(app, 'BuildStreamingAnalyticsWorkshopResources');
