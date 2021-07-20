9bf04fe27e360eb51836e6b2b65a967c3e1cd583
Steffen Hausmann <shausma@amazon.de>
2021-07-20 10:56:50 +0200

    Remove EMR and update KDA to Flink 1.11

--- resources/beam-on-kda/cdk/lib/workshop-infrastructure.ts
+++ resources/beam-on-kda/cdk/lib/workshop-infrastructure.ts
@@ -77,7 +77,7 @@ export class WorkshopInfrastructure extends cdk.Stack {
 
     const enrichEvents = new lambda.Function(this, "EnrichEventsLambda", {
       runtime: lambda.Runtime.NODEJS_12_X,
-      code: lambda.Code.inline(addTimestamplambdaSource),
+      code: lambda.Code.fromInline(addTimestamplambdaSource),
       timeout: Duration.seconds(60),
       handler: "index.handler",
     });
@@ -90,7 +90,7 @@ export class WorkshopInfrastructure extends cdk.Stack {
     const terminateAppLambda = new lambda.Function(this, "TerminateAppLambda", {
       runtime: lambda.Runtime.PYTHON_3_7,
       timeout: Duration.minutes(15),
-      code: lambda.Code.inline(stopApplicationlambdaSource),
+      code: lambda.Code.fromInline(stopApplicationlambdaSource),
       handler: "index.empty_bucket",
       memorySize: 512,
       environment: {
@@ -136,86 +136,6 @@ export class WorkshopInfrastructure extends cdk.Stack {
     s3.Bucket.fromBucketName(this, 'PublicDatasetBucket', 'nyc-tlc').grantRead(kdaRole);
 
 
-    const emrSg = new ec2.SecurityGroup(this, 'EmrSecurityGroup', {
-      vpc: vpc
-    });
-
-    emrSg.addIngressRule(sg, ec2.Port.allTraffic());
-
-    const emrClusterRole = new iam.Role(this, 'EmrClusterRole', {
-      assumedBy: new iam.ServicePrincipal('elasticmapreduce.amazonaws.com'),
-      managedPolicies: [
-          iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonElasticMapReduceRole')
-      ]
-    });
-
-    const emrInstanceRole = new iam.Role(this, 'EmrInstanceRole', {
-      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
-      managedPolicies: [
-          iam.ManagedPolicy.fromAwsManagedPolicyName('service-role/AmazonElasticMapReduceforEC2Role'),
-          iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
-      ]
-    });
-
-    const emrProfile = new iam.CfnInstanceProfile(this, 'EmrInstanceProfile', {
-        roles: [
-          emrInstanceRole.roleName
-        ]
-    });
-
-    const cluster = new emr.CfnCluster(this, 'EmrCluster', {
-      name: 'beam-workshop',
-      applications: [
-          { name: 'Hadoop' }, 
-          { name: 'Ganglia' }, 
-          { name: 'Flink' }, 
-          { name: 'ZooKeeper'}
-      ],
-      instances: {
-          emrManagedMasterSecurityGroup: emrSg.securityGroupId,
-          emrManagedSlaveSecurityGroup: emrSg.securityGroupId,
-          masterInstanceGroup: {
-              instanceCount: 1,
-              instanceType: 'c5n.xlarge',
-              name: 'Master'
-          },
-          coreInstanceGroup: {
-              instanceCount: 2,
-              instanceType: 'r5.xlarge',
-              name: 'Core'
-          },
-          ec2SubnetId: vpc.publicSubnets[0].subnetId
-      },
-      serviceRole : emrClusterRole.roleName,
-      releaseLabel: 'emr-5.27.0',
-      visibleToAllUsers: true,
-      jobFlowRole: emrProfile.ref,
-    });
-
-    const getEmrInstanceIdSource = fs
-      .readFileSync("lambda/get-emr-master-id.py")
-      .toString();
-
-    const getEmrInstanceId = new lambda.Function(this, "GetEmrInstanceIdLambda", {
-      runtime: lambda.Runtime.PYTHON_3_7,
-      code: lambda.Code.inline(getEmrInstanceIdSource),
-      timeout: Duration.seconds(30),
-      handler: "index.get_instance_id",
-    });
-
-    getEmrInstanceId.addToRolePolicy(new iam.PolicyStatement({
-      actions: ['elasticmapreduce:ListInstances'],
-      resources: [ `arn:${cdk.Aws.PARTITION}:elasticmapreduce:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:cluster/${cluster.ref}` ]
-    }));
-
-    const customResource = new cfn.CustomResource(this, 'GetEmrInstanceIdResource', {
-      provider: cfn.CustomResourceProvider.fromLambda(getEmrInstanceId),
-      properties: {
-        EmrId: cluster.ref
-      }
-    });
-
-
     const metric = (dimensions?: cloudwatch.DimensionHash) => new cloudwatch.Metric({
       metricName: 'Number of Trips',
       namespace: 'Beam',
@@ -251,8 +171,6 @@ export class WorkshopInfrastructure extends cdk.Stack {
       stacked: true
     }));
 
-    new cdk.CfnOutput(this, "ConnectToEmrCluster", { value: `https://console.aws.amazon.com/systems-manager/session-manager/${customResource.getAtt('EmrMasterInstanceId')}` });
     new cdk.CfnOutput(this, "DownloadJarFile", { value: `aws s3 cp --recursive --exclude '*' --include '${props.beamApplicationJarFile}' 's3://${bucket.bucketName}/target/' .` })
-    new cdk.CfnOutput(this, 'StartBeamApplication', { value: `flink run -p 8 ${props.beamApplicationJarFile} --runner=FlinkRunner --awsRegion=${cdk.Aws.REGION} --source=s3 --inputS3Pattern=s3://${bucket.bucketName}/historic-trip-events/*/*/*/*/* --inputStreamName=beam-workshop --outputBoroughs=true` });
   }
 }
